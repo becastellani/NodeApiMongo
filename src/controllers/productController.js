3697f824428d47abf39d620add4b2f0e7d50564b
Bernardo Castellani <becastellani10@gmail.com>
2025-03-23 17:58:03 -0300

    Order with hateos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -14,14 +14,38 @@ export const listProducts = async (req, res, next) => {
   try {
     const page = parseInt(req.query._page) || 1;
     const size = parseInt(req.query._size) || 10;
+    const sort = req.query._sort || 'name'; 
+    const order = req.query._order === 'desc' ? -1 : 1; 
+
     const offset = (page - 1) * size;
 
-    const products = await Product.find({}).skip(offset).limit(size);
+    const products = await Product.find({})
+      .skip(offset)
+      .limit(size)
+      .sort({ [sort]: order }); 
+
     const totalData = await Product.countDocuments();
     const totalPages = Math.ceil(totalData / size);
 
-    const data = res.hateos_list("products", products, totalPages);
-    res.ok(data);
+    res.ok({
+      products: products.map((product) => ({
+        ...product._doc,
+        _links: [
+          { rel: "self", href: `${req.baseUrl}/${product._id}`, method: "GET" },
+        ],
+      })),
+      _page: {
+        current: page,
+        total: totalPages,
+        size: products.length,
+      },
+      _links: [
+        { rel: "self", href: req.baseUrl, method: "GET" },
+        { rel: "create", href: req.baseUrl, method: "POST" },
+        { rel: "previous", href: page > 1 ? `${req.baseUrl}?_page=${page - 1}` : null, method: "GET" },
+        { rel: "next", href: page < totalPages ? `${req.baseUrl}?_page=${page + 1}` : null, method: "GET" },
+      ],
+    });
   } catch (err) {
     next(err);
   }
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,21 +15,47 @@ export const showUser = async (req, res, next) => {
 }
 
 export const listUsers = async (req, res, next) => {
-  const users = await User.find({});
+  try {
+    const page = parseInt(req.query._page) || 1;
+    const size = parseInt(req.query._size) || 10;
+    const sort = req.query._sort || 'name'; 
+    const order = req.query._order === 'desc' ? -1 : 1; 
 
-  res.ok({
-    users: users.map((user) => ({
-      ...user._doc,
+    const offset = (page - 1) * size;
+
+    const users = await User.find({})
+      .skip(offset)
+      .limit(size)
+      .sort({ [sort]: order });
+
+    const totalData = await User.countDocuments();
+    const totalPages = Math.ceil(totalData / size);
+
+    res.ok({
+      users: users.map((user) => ({
+        ...user._doc,
+        _links: [
+          { rel: "self", href: `${req.baseUrl}/${user._id}`, method: "GET" },
+        ],
+      })),
+      _page: {
+        current: page,
+        total: totalPages,
+        size: users.length,
+      },
       _links: [
-        { rel: "self", href: `${req.baseUrl}/${user._id}`, method: "GET" },
+        { rel: "self", href: req.baseUrl, method: "GET" },
+        { rel: "create", href: req.baseUrl, method: "POST" },
+        { rel: "previous", href: page > 1 ? `${req.baseUrl}?_page=${page - 1}` : null, method: "GET" },
+        { rel: "next", href: page < totalPages ? `${req.baseUrl}?_page=${page + 1}` : null, method: "GET" },
       ],
-    })),
-    _links: [
-      { rel: "self", href: req.baseUrl, method: "GET" },
-      { rel: "create", href: req.baseUrl, method: "POST" }
-    ],
-  });
-}
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+
 
 export const createUser = async (req, res, next) => {
   await new User(req.body).save();
